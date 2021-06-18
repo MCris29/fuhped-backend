e5183bf1e7cb4d85b627532d0f5105bd53b8cb40
MCris29 <crismax0629@gmail.com>
2021-06-17 22:51:59 -0500

    models & migrations admin, partner & afiliate

--- app/Models/Afiliate.php
+++ app/Models/Afiliate.php
@@ -2,12 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class Afiliate extends model
 {
     use HasFactory, Notifiable;
 
@@ -19,7 +18,9 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'last_name',
+        'address',
         'phone',
+        'state',
         'email',
         'password',
     ];
--- database/migrations/2021_06_17_223231_create_partners_table.php
+++ database/migrations/2021_06_17_223231_create_partners_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreatePartnersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('partners', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('last_name');
-            $table->string('phone');
+            $table->string('business');
+            $table->string('description');
+            $table->string('address');
+            $table->string('phone', 13);
+            $table->string('state', 20);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
@@ -33,6 +37,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('partners');
     }
 }
