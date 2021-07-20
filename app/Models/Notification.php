84cb019239aaba6b299e8ec4593f140a1669fc98
MCris29 <crismax0629@gmail.com>
2021-07-20 09:41:56 -0500

    Relationships

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -4,12 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class Notification extends Model
 {
     protected $fillable = [
         'title',
     ];
+
+    public static function boot()
+    {
+        parent::boot();
+        static::creating(function ($notification) {
+            $notification->user_id = Auth::id();
+        });
+    }
 
     public function transmitter()
     {
